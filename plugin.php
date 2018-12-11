51b6f95f4b04f7af77f1315a45e273e4df639378
Brent Hardinge <designerbrent@gmail.com>
2018-12-11 13:57:40 -0500

    Update to the plugin metadata file.

--- plugin.php
+++ plugin.php
@@ -1,15 +1,11 @@
 <?php
 /**
  * Plugin Name: ALPS Gutenberg Blocks
- * Plugin URI: https://github.com/ahmadawais/create-guten-block/
- * Description: Creates custom blocks in Gutenberg specific to the ALPS v3 theme. This Gutenberg plugin was created via create-guten-block.
- * Author: mrahmadawais, maedahbatool
- * Author URI: https://AhmadAwais.com/
- * Version: 1.0.0
- * License: GPL2+
- * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
- *
- * @package CGB
+ * Plugin URI: https://adventist.io/themes
+ * Description: Creates custom blocks in Gutenberg specific to the ALPS v3 theme.
+ * Author: Seventh-day Adventist Church and SouthLeft
+ * Author URI: https://adventist.io/themes
+ * Version: 1.0.1
  */
 
 // Exit if accessed directly.
