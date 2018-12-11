044637698ddfbca78ca740030bff402714edea9e
Kelsey Cahill <kcahill@southleft.com>
2018-12-11 13:45:00 -0500

    Added editor dependency

--- src/init.php
+++ src/init.php
@@ -25,7 +25,7 @@ function gutenberg_blocks_block_assets() {
 	wp_enqueue_style(
 		'gutenberg-blocks-style-css', // Handle.
 		plugins_url( 'dist/blocks.style.build.css', dirname( __FILE__ ) ), // Block style CSS.
-		array( 'wp-blocks' ) // Dependency to include the CSS after it.
+		array( 'wp-blocks') // Dependency to include the CSS after it.
 		// filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.style.build.css' ) // Version: filemtime — Gets file modification time.
 	);
 } // End function gutenberg_blocks_block_assets().
@@ -47,7 +47,7 @@ function gutenberg_blocks_editor_assets() {
 	wp_enqueue_script(
 		'gutenberg-blocks-block-js', // Handle.
 		plugins_url( '/dist/blocks.build.js', dirname( __FILE__ ) ), // Block.build.js: We register the block here. Built with Webpack.
-		array( 'wp-blocks', 'wp-i18n', 'wp-element' ), // Dependencies, defined above.
+		array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor' ), // Dependencies, defined above.
 		// filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.build.js' ), // Version: filemtime — Gets file modification time.
 		true // Enqueue the script in the footer.
 	);
