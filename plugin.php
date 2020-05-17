32ad61587e17842c062ca711362aa016266c61be
Brent Hardinge <designerbrent@gmail.com>
2020-05-17 17:06:02 -0400

    Fix the version numbering.

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package alps-gutenberg-blocks
- * @version 1.4.2
+ * @version 1.4.3
  */
 /**
  * Plugin Name: ALPS Gutenberg Blocks
@@ -9,7 +9,7 @@
  * Description: Creates custom blocks in Gutenberg specific to the ALPS v3 theme.
  * Author: Seventh-day Adventist Church and SouthLeft
  * Author URI: https://adventist.io/themes
- * Version: 1.4.
+ * Version: 1.4.3
  */
 
 // Exit if accessed directly.
