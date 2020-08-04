5711b35c42805b8d33584b477d14a3879ed42e4c
Sergii Sadovyi <1175636+sergsadovyi@users.noreply.github.com>
2020-08-04 17:16:40 +0300

    fix(gallery): not cut the image in preview

--- src/blocks/gallery/block.js
+++ src/blocks/gallery/block.js
@@ -79,7 +79,7 @@ registerBlockType( 'alps-gutenberg-blocks/gallery', {
               </h2>
               <button className="c-gallery-block__toggle js-toggle o-button o-button--outline o-button--toggle o-button--small" data-toggled="this" data-prefix="this"><span class="u-icon u-icon--xs u-path-fill--white"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 10"><title>o-icon__plus</title><path d="M10,4H6V0H4V4H0V6H4v4H6V6h4Z" fill="#9b9b9b"/></svg></span></button>
             </div>
-            <div className="c-gallery-block__thumb u-background--cover" style={ { backgroundImage: `url('${ images.map( ( image, index ) => image.url )[0] }');` } }>
+            <div className="c-gallery-block__thumb u-background--contain" style={ { backgroundImage: `url('${ images.map( ( image, index ) => image.url )[0] }');` } }>
             </div>
           </div>
 
