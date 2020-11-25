8e96e7a90ce6b2fbf18bd7140747e3996870d6d9
YauheniKapliarchuk <Yauheni.Kapliarchuk@gmail.com>
2020-11-26 00:40:55 +0300

    [511] Inline Sidebar. Add empty structure.

---
+++ src/blocks/inline-sidebar/block.js
@@ -0,0 +1,30 @@
+/**
+ * BLOCK: Inline Sidebar
+ */
+
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import { registerBlockType } from '@wordpress/blocks';
+
+/**
+ * Internal dependencies
+ */
+import edit from './components/edit';
+import save from './components/save';
+// import './style.scss';
+
+registerBlockType('alps-gutenberg-blocks/inline-sidebar', {
+    title: __('ALPS Inline Sidebar', 'alps-gutenberg-blocks'),
+    description: __('Flexible media content.', 'alps-gutenberg-blocks'),
+    icon: 'format-image',
+    category: 'common',
+    attributes: {
+        title: {
+            type: 'string',
+        },
+    },
+    edit,
+    save,
+});
---
+++ src/blocks/inline-sidebar/components/edit.js
@@ -0,0 +1,5 @@
+export default () => {
+    return (
+        <div>Inline Sidebar Block Edit</div>
+    );
+}
