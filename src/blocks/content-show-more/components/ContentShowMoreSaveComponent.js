9861b338d248836ef21d6152a4d606feff3aae4c
Betsy Andrew <betsygunio@gmail.com>
2022-10-31 11:10:59 -0400

    fix display

--- src/blocks/content-show-more/components/ContentShowMoreSaveComponent.js
+++ src/blocks/content-show-more/components/ContentShowMoreSaveComponent.js
@@ -1,32 +1,39 @@
-import {Component} from "@wordpress/element";
-import cls from 'classnames';
+import { Component } from "@wordpress/element";
+import cls from "classnames";
 
 export class ContentShowMoreSaveComponent extends Component {
-    render () {
+	render() {
+		const { attributes } = this.props;
 
-        const { attributes } = this.props;
+		const styles = cls(
+			"c-block c-content-read-more c-block__text u-theme--border-color--darker u-border--left c-block__text-expand u-spacing u-background-color--gray--light u-padding u-clear-fix can-be--dark-dark",
+			{ "has-image": attributes.imageURL }
+		);
 
-        const styles = cls(
-            'c-block c-block__text u-theme--border-color--darker u-border--left c-block__text-expand u-spacing u-background-color--gray--light u-padding u-clear-fix can-be--dark-dark',
-            {'has-image': attributes.imageURL}
-        )
-
-        return (
-            <div>
-                <div className={ styles }>
-                    { attributes.imageURL &&
-                        <img className="c-block__image" src={ `${ attributes.imageURL }` } />
-                    }
-                    <h3 className="u-theme--color--darker u-font--primary--m">
-                        <strong>{ attributes.title }</strong>
-                    </h3>
-                    <p className="c-block__body text o-description" style={ { textAlign: attributes.alignment } }>{ attributes.description }</p>
-                    <div className="c-block__content">
-                        <p className="o-paragraph" style={ { textAlign: attributes.alignment } }>{ attributes.body }</p>
-                    </div>
-                    <a className="o-button o-button--outline o-button--expand js-toggle-parent"></a>
-                </div>
-            </div>
-        );
-    }
+		return (
+			<div>
+				<div className={styles}>
+					{attributes.imageURL && (
+						<img className="c-block__image" src={`${attributes.imageURL}`} />
+					)}
+					<h3 className="u-theme--color--darker u-font--primary--m">
+						<strong>{attributes.title}</strong>
+					</h3>
+					<p
+						className="c-block__body text o-description"
+						style={{ textAlign: attributes.alignment }}
+					>
+						{attributes.description}
+					</p>
+					<p
+						className="o-paragraph"
+						style={{ textAlign: attributes.alignment }}
+					>
+						{attributes.body}
+					</p>
+					<a className="o-button o-button--outline o-button--expand js-toggle-parent"></a>
+				</div>
+			</div>
+		);
+	}
 }
